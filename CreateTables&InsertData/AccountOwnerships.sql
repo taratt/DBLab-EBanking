b52a0c2492b25655471f7cd146c6aa3570a1d842
taratt <51206120+taratt@users.noreply.github.com>
2022-04-29 02:47:29 +0430

    Create AccountOwnerships.sql

---
+++ CreateTables&InsertData/AccountOwnerships.sql
@@ -0,0 +1,8 @@
+CREATE TABLE AccountOwnerships (
+    ownership_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT NOT NULL,
+	cutomer_id INT NOT NULL,
+	FOREIGN KEY (account_id) REFERENCES Accounts(id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(id),
+	
+);
